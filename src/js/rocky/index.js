c6ca1e35a6ca30736c22e206cf11c50c99b7c453
Jon Barlow <orviwan@gmail.com>
2016-07-29 18:06:05 +0100

    optimised

--- src/js/rocky/index.js
+++ src/js/rocky/index.js
@@ -3,22 +3,22 @@ var rocky = _rocky;
 var monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 
   'July', 'August', 'September', 'October', 'November', 'December'];
 
+var drawReason = {init: true};
+
 rocky.on('draw', function(drawEvent) {
+
   var ctx = drawEvent.context;
   var w = ctx.canvas.unobstructedWidth;
   var h = ctx.canvas.unobstructedHeight;
   var obstruction_h = ctx.canvas.clientHeight - ctx.canvas.unobstructedHeight;
   var is_round = false;
+  var d = new Date();
 
   // Sorry Heiko
   if (ctx.canvas.clientWidth == 180) { // TODO: WatchInfo.model.name == ??
     // CHALK
     is_round = true;
   }
-
-  ctx.clearRect(0, 0, ctx.canvas.clientWidth, ctx.canvas.clientHeight);
-
-  var d = new Date();
 
   ctx.fillStyle = '#FFF';
   if (is_round) {
@@ -27,27 +27,49 @@ rocky.on('draw', function(drawEvent) {
     ctx.textAlign = 'left';
   }
 
-  // TIME
-  var clockTime = leftpad(d.getHours(), 2, 0) + ':' + leftpad(d.getMinutes(), 2, 0); // TODO: Detect 24h
-  ctx.font = '49px Roboto-subset';
-  ctx.fillText(clockTime, ((is_round) ? w / 2 : 7), 92 - obstruction_h);
+  if (drawReason.init) {
+    // BLANK CANVAS
+    ctx.clearRect(0, 0, ctx.canvas.clientWidth, ctx.canvas.clientHeight);
 
-  // HORIZONTAL LINE
-  if(is_round) {
-    ctx.fillRect(0, 97 - obstruction_h, w, 2); // full width
-  } else {
-    ctx.fillRect(8, 97 - obstruction_h, w - 16, 2); // indented
+    // HORIZONTAL LINE
+    if(is_round) {
+      ctx.fillRect(0, 97 - obstruction_h, w, 2); // full width
+    } else {
+      ctx.fillRect(8, 97 - obstruction_h, w - 16, 2); // indented
+    }
   }
 
-  // DATE
-  var clockDate = monthNames[d.getMonth()] + ' ' + d.getDate()
-  ctx.font = '21px Roboto';
-  ctx.fillText(clockDate, ((is_round) ? w / 2 : 8), 68 - obstruction_h);
+  if (drawReason.minutechange) {
+    // TIME
+    ctx.clearRect(0, 103, ctx.canvas.clientWidth, 40);
+    var clockTime = leftpad(d.getHours(), 2, 0) + ':' + leftpad(d.getMinutes(), 2, 0); // TODO: Detect 24h
+    ctx.font = '49px Roboto-subset';
+    ctx.fillText(clockTime, ((is_round) ? w / 2 : 7), 92 - obstruction_h);
+  }
 
+  if (drawReason.daychange) {
+    // DATE
+    ctx.clearRect(0, 70, ctx.canvas.clientWidth, 27);
+    var clockDate = monthNames[d.getMonth()] + ' ' + d.getDate()
+    ctx.font = '21px Roboto';
+    ctx.fillText(clockDate, ((is_round) ? w / 2 : 8), 68 - obstruction_h);
+
+  }
+
+  drawReason = {};
 });
 
-rocky.on('minutechange', function(e) {
+var triggerDraw = function(e) {
+  drawReason[e.type] = true;
   rocky.requestDraw();
+}
+
+rocky.on('minutechange', function() {
+  triggerDraw({type: 'minutechange'});
+});
+
+rocky.on('daychange', function() {
+  triggerDraw({type: 'daychange'});
 });
 
 function leftpad(str, len, ch) {
